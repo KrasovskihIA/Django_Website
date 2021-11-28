53f294d345d181cc96cc8282e8145d0b3d4f11a1
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2021-11-28 22:23:53 +0400

    Шаблон главной страницы
    The template of the main page with the output of movies

--- MovieDjango/movies/views.py
+++ MovieDjango/movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import *
 
-# Create your views here.
+
+class MovieView(View):
+    def get(self, reqest):
+        movies = Movie.objects.all()
+        return render(reqest, 'movies/movies.html', {'movie_list': movies})
+

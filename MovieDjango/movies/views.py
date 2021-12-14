51b7f17efada43aa88e37d7874e2128fa728496f
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2021-12-14 21:30:47 +0400

    Комментарии, кадры
    Настройка комментариев и кадров из фильма

--- MovieDjango/movies/views.py
+++ MovieDjango/movies/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from .models import *
+from .forms import ReviewForms
 
 
 class MovieView(ListView):
@@ -13,3 +14,13 @@ class MovieView(ListView):
 class MovieDetailView(DetailView):
     model = Movie
     slug_field = 'url'
+
+class AddReview(View):
+    def post(self, request, pk):
+        form = ReviewForms(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie_id = pk
+            form.save()
+        return redirect(movie.get_absolute_url())

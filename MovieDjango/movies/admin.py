b775dbaf6bba308d22e8286ab92d9903390fbf0a
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2021-12-25 20:52:33 +0400

    ckeditor + plugin youtube
     Использование редактора ckeditor в админке django и plugin youtube

--- MovieDjango/movies/admin.py
+++ MovieDjango/movies/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from .models import *
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(label ='Описание', widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+
 
 #Класс Категорий
 @admin.register(Category)
@@ -36,6 +49,7 @@ class MovieAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ('draft',)
+    form = MovieAdminForm
     readonly_fields = ('get_image',)
     fieldsets = (
         (None, {

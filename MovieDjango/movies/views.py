de5ea05534696a5b47afc94367d1fdbf7fb1b103
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2021-12-26 23:44:33 +0400

    Фильтрация фильмов по годам и жанрам
    Filtering movies by year and genre

--- MovieDjango/movies/views.py
+++ MovieDjango/movies/views.py
@@ -3,15 +3,25 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from .models import *
 from .forms import ReviewForms
+from django.db.models import Q
 
 
-class MovieView(ListView):
+#Жанры и года выхода фильмов
+class GenryYear:
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values('year')
+
+
+class MovieView(GenryYear, ListView):
     model= Movie
     queryset = Movie.objects.filter(draft=False)
 
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenryYear, DetailView):
     model = Movie
     slug_field = 'url'
 
@@ -30,7 +40,18 @@ class AddReview(View):
 
 
 
-class ActorView(DetailView):
+class ActorView(GenryYear, DetailView):
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = 'name'
+
+
+
+#Фильтры фильмов
+class FilterMovieView(GenryYear, ListView):
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(genres__in=self.request.GET.getlist("genre"))
+        )
+        return queryset
